6c19105a39c442fe83181e142bcb02c17be22ba9
thiagomathiassimon <mathiastsimon@gmail.com>
2022-04-26 17:25:59 -0300

    WIP: adicionadas algumas classes

---
+++ acme/src/br/com/acme/model/BadeiraCartao.java
@@ -0,0 +1,8 @@
+package br.com.acme.model;
+
+public class BadeiraCartao {
+
+    private String bandeira;
+    private String numeroCartao;
+
+}
--- acme/src/br/com/acme/model/FormaPagamento.java
+++ acme/src/br/com/acme/model/FormaPagamento.java
@@ -1,4 +1,11 @@
 package br.com.acme.model;
 
+import java.util.List;
+
 public class FormaPagamento {
+
+    private String tipo;
+    private int numeroParcelas;
+    private List<BadeiraCartao> badeiraCartao;
+
 }
